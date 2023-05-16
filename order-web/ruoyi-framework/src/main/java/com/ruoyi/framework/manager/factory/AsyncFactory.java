4bd8453638ab8df371f92b333e97a9ec695ebcbc
zhangjiwei <1945192314@qq.com>
2023-05-16 12:31:12 +0800

    feat: init web

---
+++ order-web/ruoyi-framework/src/main/java/com/ruoyi/framework/manager/factory/AsyncFactory.java
@@ -0,0 +1,33 @@
+package com.ruoyi.framework.manager.factory;
+
+import com.ruoyi.common.utils.ip.AddressUtils;
+import com.ruoyi.common.utils.spring.SpringUtils;
+import com.ruoyi.system.domain.SysOperLog;
+import com.ruoyi.system.service.ISysOperLogService;
+
+import java.util.TimerTask;
+
+/**
+ * 异步工厂（产生任务用）
+ *
+ * @author ruoyi
+ */
+public class AsyncFactory {
+
+    /**
+     * 操作日志记录
+     *
+     * @param operLog 操作日志信息
+     * @return 任务task
+     */
+    public static TimerTask recordOper(final SysOperLog operLog) {
+        return new TimerTask() {
+            @Override
+            public void run() {
+                // 远程查询操作地点
+                operLog.setOperLocation(AddressUtils.getRealAddressByIP(operLog.getOperIp()));
+                SpringUtils.getBean(ISysOperLogService.class).insertOperlog(operLog);
+            }
+        };
+    }
+}
